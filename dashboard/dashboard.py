f8570376ab321cfb96557025bc4b63af83359cb6
lucasosiewicz <osiewicz87@gmail.com>
2024-08-30 12:21:12 +0200

    Added plot of cars currently on parking

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -1,4 +1,5 @@
 import os
+from style import *
 import pandas as pd
 import plotly.express as px
 from datetime import datetime
@@ -19,23 +20,27 @@ DB_URL = f'postgresql://{DB_USERNAME}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME
 engine = create_engine(DB_URL)
 data = pd.read_sql('SELECT * FROM plates_platepaid', engine)
 
+# CONVERTING INTO RIGHT DATA FORMAT
+data['arrived_at'] = data['arrived_at'].dt.tz_convert('Europe/Warsaw')
+data['paid_at'] = data['paid_at'].dt.tz_convert('Europe/Warsaw')
 
 # DASH APP
 app = Dash(__name__)
 app.layout = html.Div([
-                dcc.DatePickerSingle(id='date', display_format='YYYY-MM-DD', date=datetime.now().date(), 
-                                     style={'position': 'realtive', 'align': 'center'}),
-                dcc.Graph(id='cars_per_hour', style={'height': '75%'}),
-            ], style={
-                    'position': 'fixed',
-                    'top': '0',       
-                    'right': '0',        
-                    'display': 'flex'   
-              })
+                html.Div([
+                    dcc.Graph(id='cars_per_hour'),
+                    dcc.DatePickerSingle(id='date_hour', display_format='YYYY-MM-DD', date=datetime.now().date()),
+                ], style=cars_per_hour_style),
+                html.Div([
+                    dcc.Graph(id='cars_on_parking'),
+                    dcc.DatePickerSingle(id='date_parking', display_format='YYYY-MM-DD', date=datetime.now().date()),
+                ], style=cars_already_parked_style),
+            ]
+        )
 
 # FUNCTIONS
 @app.callback(Output('cars_per_hour', 'figure'), 
-              Input('date', 'date'))
+              Input('date_hour', 'date'))
 def cars_per_hour(date):
 
     # If date isn't specified, use today's date
@@ -50,20 +55,19 @@ def cars_per_hour(date):
 
     # Grouping by hour
     all_hours = pd.DataFrame({'hour': range(24)})
-    df_count = df_hours.groupby(df_hours['arrived_at'].dt.hour).size().reset_index()
-    df_count.columns = ['hour', 'count']
-
-    # Merge all hours with the count
-    df = pd.merge(all_hours, df_count, on='hour', how='left').fillna(0)
+    for i, row in all_hours.iterrows():
+        hour = row['hour']
+        all_hours.at[i, 'count'] = len(df_hours[df_hours['arrived_at'].dt.hour == hour])
 
     # Plotting
-    fig = px.bar(df, 
+    fig = px.bar(all_hours, 
                  x='hour', 
-                 y='count'
+                 y='count',
+                 text_auto=True,
             )
     fig.update_layout(
         title={
-            'text': 'Number of cars per hour',
+            'text': 'Number of cars arriving per hour',
             'x': 0.5,
             'xanchor': 'center',
             'yanchor': 'top'
@@ -72,10 +76,50 @@ def cars_per_hour(date):
         yaxis_title='Number of cars',
     )
     fig.update_xaxes(tickvals=list(range(24)))
-    fig.update_yaxes(tickvals=list(range(0, df['count'].astype(int).max() + 1)))
+    fig.update_yaxes(tickvals=list(range(all_hours['count'].astype(int).max() + 1)))
     return fig
 
 
+@app.callback(Output('cars_on_parking', 'figure'), 
+              Input('date_parking', 'date'))
+def cars_on_parking(date):
+
+    if date is None:
+        date = datetime.now().date()
+
+    date = datetime.strptime(date, '%Y-%m-%d').date()
+
+    # Fetching data from the current day
+    df_hours = data[(data['arrived_at'].dt.date >= date) & (data['paid_at'].dt.date <= date)]
+
+    # Grouping by hour
+    all_hours = pd.DataFrame({'hour': range(24)})
+
+    # Counting cars on parking per hour
+    for i, row in all_hours.iterrows():
+        hour = row['hour']
+        all_hours.at[i, 'count'] = len(df_hours[(df_hours['arrived_at'].dt.hour <= hour) & (df_hours['paid_at'].dt.hour >= hour)])
+
+    # Plotting
+    fig = px.bar(all_hours, 
+                 x='hour', 
+                 y='count',
+                 text_auto=True,
+            )
+    fig.update_layout(
+        title={
+            'text': 'Number of cars currently at the parking per hour',
+            'x': 0.5,
+            'xanchor': 'center',
+            'yanchor': 'top'
+        },
+        xaxis_title='Hour',
+        yaxis_title='Number of cars',
+    )
+    fig.update_xaxes(tickvals=list(range(24)))
+    fig.update_yaxes(tickvals=list(range(0, all_hours['count'].astype(int).max() + 1, 4)))
+
+    return fig
 
 
 
---
+++ dashboard/style.py
@@ -0,0 +1,15 @@
+cars_per_hour_style = {
+    'position': 'absolute',
+    'top': '0',
+    'right': '0',
+    'width': '50%',
+    'height': '50%',
+}
+
+cars_already_parked_style = {
+    'position': 'absolute',
+    'top': '0',
+    'left': '0',
+    'width': '50%',
+    'height': '50%',
+}
